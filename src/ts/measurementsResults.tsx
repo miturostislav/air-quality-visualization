10ddeb95d3b2fe672c418308db47fb26940c70dd
Mitu Rostislav <rmitu@tacitknowledge.com>
2019-11-08 20:50:50 +0200

    Style measurements results on mobile

--- src/ts/measurementsResults.tsx
+++ src/ts/measurementsResults.tsx
@@ -28,19 +28,19 @@ function MeasurementsResults({ location, goBack }: MeasurementsResultsProps) {
 
   return (
     <div className="location-data-page">
-      <button className="location-data-page__back-button" onClick={goBack}>Go back</button>
+      <button className="location-data-page__back-button hide-xs-only" onClick={goBack}>Go back</button>
       <table className="location-data-page__results">
         <caption className="a11y-invisible-element">Location measurements</caption>
-        <thead>
+        <thead className="a11y-invisible-element-xs-only">
           <tr role="row" className="location-data-page__item">
-            <th role="columnheader" className="location-data-page__location" scope="col">Location</th>
-            <th role="columnheader" className="location-data-page__city" scope="col">City</th>
-            <th role="columnheader" className="location-data-page__country" scope="col">Country</th>
-            <th role="columnheader" className="location-data-page__parameter" scope="col">Parameter</th>
-            <th role="columnheader" className="location-data-page__value" scope="col">Value</th>
-            <th role="columnheader" className="location-data-page__unit" scope="col">Unit</th>
-            <th role="columnheader" className="location-data-page__coordinates" scope="col">Coordinates</th>
-            <th role="columnheader" className="location-data-page__date" scope="col">Date</th>
+            <th role="columnheader" className="location-data-page__location location-data-page__field" scope="col">Location</th>
+            <th role="columnheader" className="location-data-page__city location-data-page__field" scope="col">City</th>
+            <th role="columnheader" className="location-data-page__country location-data-page__field" scope="col">Country</th>
+            <th role="columnheader" className="location-data-page__parameter location-data-page__field" scope="col">Parameter</th>
+            <th role="columnheader" className="location-data-page__value location-data-page__field" scope="col">Value</th>
+            <th role="columnheader" className="location-data-page__unit location-data-page__field" scope="col">Unit</th>
+            <th role="columnheader" className="location-data-page__coordinates location-data-page__field" scope="col">Coordinates</th>
+            <th role="columnheader" className="location-data-page__date location-data-page__field" scope="col">Date</th>
           </tr>
         </thead>
         <tbody>
@@ -52,15 +52,23 @@ function MeasurementsResults({ location, goBack }: MeasurementsResultsProps) {
 
               return (
                 <tr role="row" key={measurement.date.local} className="location-data-page__item">
-                  <td role="cell" className="location-data-page__location">{measurement.location}</td>
-                  <td role="cell" className="location-data-page__city">{measurement.city}</td>
-                  <td role="cell" className="location-data-page__country">{measurement.country}</td>
-                  <td role="cell" className="location-data-page__parameter">{measurement.parameter}</td>
-                  <td role="cell" className="location-data-page__value">{measurement.value}</td>
-                  <td role="cell" className="location-data-page__unit">{measurement.unit}</td>
-                  <td role="cell" className="location-data-page__coordinates">
+                  <span className="location-data-page__col-header hide-sm-up" aria-hidden>Location</span>
+                  <td role="cell" className="location-data-page__location location-data-page__field">{measurement.location}</td>
+                  <span className="location-data-page__col-header hide-sm-up" aria-hidden>City</span>
+                  <td role="cell" className="location-data-page__city location-data-page__field">{measurement.city}</td>
+                  <span className="location-data-page__col-header hide-sm-up" aria-hidden>Country</span>
+                  <td role="cell" className="location-data-page__country location-data-page__field">{measurement.country}</td>
+                  <span className="location-data-page__col-header hide-sm-up" aria-hidden>Parameter</span>
+                  <td role="cell" className="location-data-page__parameter location-data-page__field">{measurement.parameter}</td>
+                  <span className="location-data-page__col-header hide-sm-up" aria-hidden>Value</span>
+                  <td role="cell" className="location-data-page__value location-data-page__field">{measurement.value}</td>
+                  <span className="location-data-page__col-header hide-sm-up" aria-hidden>Unit</span>
+                  <td role="cell" className="location-data-page__unit location-data-page__field">{measurement.unit}</td>
+                  <span className="location-data-page__col-header hide-sm-up" aria-hidden>Coordinates</span>
+                  <td role="cell" className="location-data-page__coordinates location-data-page__field">
                     {measurement.coordinates.latitude.toFixed(4)}, {measurement.coordinates.longitude.toFixed(4)}
                   </td>
+                  <span className="location-data-page__col-header hide-sm-up" aria-hidden>Dates</span>
                   <td role="cell" className="location-data-page__date">{formattedDate}</td>
                 </tr>
               )
